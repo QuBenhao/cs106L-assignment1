b50e1b168ce615cd6c29a2078d1a3b22375875a2
benhao <qubenhao2@gmail.com>
2025-06-19 20:42:25 +0800

    feat: add ignore

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ Assignment #1 for CS106L. Check out our course website [here](http://cs106l.stan
 ## resources
 
 [课程链接](https://web.stanford.edu/class/archive/cs/cs106l/cs106l.1212/assignments.html)
+
+## commands
+
+dockerfile from the parent directory
+
+```bash
+docker build -t cs106l .
+```
+
+```bash
+docker run -it --name cs106l -v $(pwd)/cs106L-assignment1:/assignment1 \
+-v $(pwd)/cs106L-assignment2:/assignment2 \
+-p 30022:22 cs106l
+```
--- WikiRacerLinks/src/main.cpp
+++ WikiRacerLinks/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_set>
+#include <algorithm>
+#include <iterator>
 
 using std::cout;            using std::endl;
 using std::string;          using std::unordered_set;
@@ -12,9 +14,25 @@ using std::string;          using std::unordered_set;
  * does that sound familiar at all?
  */
 unordered_set<string> findWikiLinks(const string& page_html) {
-    // TODO: delete this return statement and implement the
-    //       function!
-    return {};
+    unordered_set<string> links;
+    auto it = page_html.begin();
+    while (it != page_html.end()) {
+        it = std::search(it, page_html.end(), "<a href=\"");
+        if (it == page_html.end()) break; // No more links found
+
+        it += 9; // Move past "<a href=\""
+        auto end_it = std::find(it, page_html.end(), '"'); // Find the closing quote
+        if (end_it != page_html.end()) {
+            string link(it, end_it);
+            if (link.find("/wiki/") == 0 && link.find(":") == string::npos) { // Check if it's a valid wiki link
+                links.insert(link);
+            }
+            it = end_it + 1; // Move past the closing quote
+        } else {
+            break; // No closing quote found, exit loop
+        }
+    }
+    return links;
 }
 
 int main() {
@@ -33,6 +51,12 @@ int main() {
     //       a single string of data called page_html (declared above)
 
     // Write code here
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "Error opening file: " << filename << endl;
+        return 1;
+    }
+    page_html.assign((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
     unordered_set<string> validLinks = findWikiLinks(page_html);
 
@@ -42,6 +66,9 @@ int main() {
     //       under the /res folder.
 
     // Write code here
+    for (const auto& link : validLinks) {
+        cout << link << endl;
+    }
 
     return 0;
 }
